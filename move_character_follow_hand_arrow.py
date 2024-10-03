4fbc26c061fa2637e03fc6346bf1ec2c9d97e80e
hyeon45 <gahyeon4585@daum.net>
2024-10-03 20:52:15 +0900

    캐릭터가 손을 따라서 움직이고, 다시 손가락이 랜덤적으로 생성되어 캐릭터는 손을 따라감

--- move_character_follow_hand_arrow.py
+++ move_character_follow_hand_arrow.py
@@ -20,23 +20,43 @@ def handle_events():
                 running = False
 
 def draw_hand_arrow():
-    arrow_x = random.randint(0, TUK_WIDTH)
-    arrow_y = random.randint(0, TUK_HEIGHT)
     hand_arrow.draw(arrow_x, arrow_y)
+
+def character_line(t):
+    global x, y, arrow_x, arrow_y
+
+    move_character_x = (1 - t) * x + t * arrow_x
+    move_character_y = (1 - t) * y + t * arrow_y
+    return move_character_x, move_character_y
+
+def character_move_to_arrow():
+    global x, y, arrow_x, arrow_y, frame
+
+    for i in range(0, 100+1):
+        t = i / 100
+        move_character_x, move_character_y = character_line(t)
+
+        clear_canvas()
+        TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+        draw_hand_arrow()
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, move_character_x, move_character_y)
+        update_canvas()
+        handle_events()
+        frame = (frame + 1) % 8
+        delay(0.05)
 
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+arrow_x = random.randint(0, TUK_WIDTH)
+arrow_y = random.randint(0, TUK_HEIGHT)
 frame = 0
 dir = 0
 
 while running:
-    clear_canvas()
-    TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    draw_hand_arrow()
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
-    update_canvas()
-    frame = (frame + 1) % 8
+    character_move_to_arrow()
 
-    handle_events()
+    x, y = arrow_x, arrow_y
+    arrow_x = random.randint(0, TUK_WIDTH)
+    arrow_y = random.randint(0, TUK_HEIGHT)
 
 close_canvas()
