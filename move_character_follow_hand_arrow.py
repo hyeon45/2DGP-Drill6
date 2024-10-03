72d250a3307377cf36795fde67b8ef96f96b80d0
hyeon45 <gahyeon4585@daum.net>
2024-10-03 15:45:38 +0900

    손을 랜덤적인 위치로 생성 및 esc키나 X버튼을 누르면 해당 프로그램을 종료시킴

--- move_character_follow_hand_arrow.py
+++ move_character_follow_hand_arrow.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import random
 
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
@@ -8,16 +9,30 @@ character = load_image('animation_sheet.png')
 hand_arrow = load_image('hand_arrow.png')
 
 def handle_events():
-    pass
+    global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                running = False
+
+def draw_hand_arrow():
+    arrow_x = random.randint(0, TUK_WIDTH)
+    arrow_y = random.randint(0, TUK_HEIGHT)
+    hand_arrow.draw(arrow_x, arrow_y)
 
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 frame = 0
+dir = 0
 
 while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    hand_arrow.draw(TUK_WIDTH // 4, TUK_HEIGHT // 4)
+    draw_hand_arrow()
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
     update_canvas()
     frame = (frame + 1) % 8
